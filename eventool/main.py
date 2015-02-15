78633e3111757f7ba5a8b6f559440dd78a35e541
yfried <yfried@redhat.com>
2015-02-15 13:09:06 +0100

    Revert "Merge tag '0.0.1'"
    This reverts commit 80619dbd06563d57e872f12d26da142240ffc318.
    
    Change-Id: I17f01171d7539457cf408f1b49ff1970799f9edb

--- eventool/main.py
+++ eventool/main.py
@@ -9,7 +9,6 @@ from eventool import hosts
 from eventool import pcs
 from eventool import ssh_cmds
 from eventool import servicemgmt
-from eventool import version
 
 
 LOG = logger.getLogger(__name__)
@@ -22,9 +21,6 @@ def parse_arguments():
                              "single host. host_role is also possible to "
                              "work on "
                              "multiple matching hosts")
-    parser.add_argument("--version", action='version',
-                        version=version.version_string())
-    # parser.set_defaults(func=lambda: version.version_string())
     subparse = parser.add_subparsers()
 
     # scripts
